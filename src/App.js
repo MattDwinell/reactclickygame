1d623e6fe76409fc4ca023cf63a15278b24e2d16
Matt D <dwinell.matt@gmail.com>
2019-07-27 13:54:48 -0400

    finished coding basic game

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import shuffle from "./friendSorter";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
+    unchosenFriends: possibleFriends,
     clicked:[false, false, false, false, false, false, false, false, false, false, false, false],
     score:0,
     highScore: 0
@@ -17,7 +18,44 @@ class App extends Component {
 
   clickCard = id => {
     console.log(id);
-    console.log(this.props);
+    const origFriendLength = this.state.unchosenFriends.length;
+    const friendsRemaining = this.state.unchosenFriends.filter(friend=>friend.id !== id);
+    const newFriendLength = friendsRemaining.length
+    const currentHighScore = this.state.highScore;
+    console.log(origFriendLength, newFriendLength);
+    if (origFriendLength !== newFriendLength){
+      if (this.state.score >= currentHighScore){ 
+    this.setState({
+      unchosenFriends: friendsRemaining,
+      score: this.state.score + 1,
+      highScore: (currentHighScore + 1)
+    })
+  } else {
+    this.setState({
+      unchosenFriends: friendsRemaining,
+      score: this.state.score + 1
+    })
+  } 
+  }else if (origFriendLength === newFriendLength){
+    alert('you have lost');
+    this.setState({
+      unchosenFriends: possibleFriends,
+      score: 0
+    })
+
+  }
+    // if (origFriendLength === newFriendLength){
+    //   console.log('user has lost');
+    // } else {
+    //   this.setState({score: (this.state.score + 1)});
+    //   if (this.state.score > this.state.highScore){
+    //     this.setState({
+    //       highScore: this.state.score
+    //     })
+      
+    //   //need to reshuffle cards here/ call reshuffle function
+    // }  
+    // }
 //need to run some logic to determine if the id has been clicked or not, if false, then evaluate to true and run randomizer function. if true, game end/reset.
 
     // Filter this.state.friends for friends with an id not equal to the id being removed
