c27e4475f2647c89dd9a2b8504d419f6fa776fd9
Matt D <dwinell.matt@gmail.com>
2019-07-27 14:13:12 -0400

    Updates

---
+++ precache-manifest.378d14d166dddb5c215c3c8747b002a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2204c30adc3b400c318775b2f705a38b",
+    "url": "/reactclickygame/index.html"
+  },
+  {
+    "revision": "8fe2bf895ee37c6e32f2",
+    "url": "/reactclickygame/static/css/main.d7bceb84.chunk.css"
+  },
+  {
+    "revision": "b01ed4a10e4d94239f2e",
+    "url": "/reactclickygame/static/js/2.84afd5aa.chunk.js"
+  },
+  {
+    "revision": "8fe2bf895ee37c6e32f2",
+    "url": "/reactclickygame/static/js/main.26a1eedd.chunk.js"
+  },
+  {
+    "revision": "3bda9a3765674e2eb487",
+    "url": "/reactclickygame/static/js/runtime~main.af3f12b9.js"
+  }
+]);
